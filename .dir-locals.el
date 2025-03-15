7cb362495c106fb86f97482c35685f509889932c
Kiso Katsuyuki <katsuyuki2388@gmail.com>
2025-03-15 16:13:34 +0900

    publish and blog configured

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,15 @@
-((nil . ((org-export-body-only . t)
-	 (org-html-toplevel-hlevel . 1))))
+((nil . ((org-html-toplevel-hlevel . 1)
+	 (eval . (let ((proot (project-root (project-current)))) ;vc-root-dir function doesn't work maybe because vc elisp files have not been loaded.
+		   (setq org-publish-project-alist
+			 `(("mypage" :components ("root" "posts"))
+			   ("root"
+			    :base-directory ,proot
+			    :publishing-directory ,proot
+			    :publishing-function org-html-publish-to-html
+			    :body-only t)
+			   ("posts"
+			    :base-directory ,(concat proot "_org/")
+			    :publishing-directory ,(concat proot "_posts")
+			    :publishing-function org-html-publish-to-html
+			    :body-only t))))
+	       ))))
