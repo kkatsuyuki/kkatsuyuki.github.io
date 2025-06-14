075841781859f46e2fcd619a35930afe5fb9aa7a
Kiso Katsuyuki <katsuyuki2388@gmail.com>
2025-06-14 22:16:25 +0900

    changed the previous software pages to posts
    date added to software posts

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,8 +1,8 @@
-((nil . ((org-html-toplevel-hlevel . 1)
+((nil . ((org-html-toplevel-hlevel . 2)
 	 (org-export-with-toc . nil)
 	 (eval . (let ((proot (project-root (project-current)))) ;vc-root-dir function doesn't work maybe because vc elisp files have not been loaded.
 		   (setq org-publish-project-alist
-			 `(("mypage" :components ("root" "posts"))
+			 `(("mypage" :components ("root" "posts" "software-posts"))
 			   ("root"
 			    :base-directory ,proot
 			    :publishing-directory ,proot
@@ -13,5 +13,11 @@
 			    :base-directory ,(concat proot "_org/")
 			    :publishing-directory ,(concat proot "_posts")
 			    :publishing-function org-html-publish-to-html
-			    :body-only t))))
+			    :body-only t)
+			   ("software-posts"
+			    :base-directory ,(concat proot "_org/software/")
+			    :publishing-directory ,(concat proot "software/_posts")
+			    :publishing-function org-html-publish-to-html
+			    :body-only t)
+			   )))
 	       ))))
